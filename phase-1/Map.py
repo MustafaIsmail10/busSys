5797f49a4dd1ef0502027dc983f02861a56249dd
Mustafa Ismail <mustafaismael10@hotmail.com>
2023-04-12 18:17:56 +0300

    Shortest path is done

--- phase-1/Map.py
+++ phase-1/Map.py
@@ -1,11 +1,12 @@
 import json
 from math import sqrt
+from uuid import uuid4
+import heapq
 
 class Map:
     def __init__(self, **kwargs):
         data = None
         self.uids = 0
-
         if "path" in kwargs.keys():
             with open(kwargs["path"], "r") as file:
                 data = json.load(file)
@@ -14,89 +15,121 @@ class Map:
             data = json.loads(kwargs["json"])
         else:
             raise Exception("omg man your data is not valide")
-        
-        self.ways = data["ways"]
-        # Parsing nodes 
+
+        # Parsing ways
+        ws = data["ways"]
+        self.ways = {}
+        for key in ws:
+            self.ways[int(key)] = ws[key]
+
+
+
+        # Parsing nodes
         ns = data["nodes"]
         self.nodes = {}
         for key in ns:
-            self.nodes[key] = {}
-            self.nodes[key]["location"] = ns[key]
-            self.nodes[key]["edges"] = []
-        
-        # Parsing edges 
+            newKey = int(key)
+            self.nodes[newKey] = {}
+            self.nodes[newKey]["location"] = ns[key]
+            self.nodes[newKey]["edges"] = []
+
+        # Parsing edges
         eds = data["edges"]
         self.edges = {}
         for key in eds:
             singleEdges = eds[key]
+            newKey = int(key)
             for edge in singleEdges:
                 newEdge = {}
-                newEdge["from"] = key
-                newEdge["to"] = edge["to"]
-                newEdge["way"] = str(edge["way"])
+                newEdge["from"] = newKey
+                newEdge["to"] = int(edge["to"])
+                newEdge["way"] = edge["way"]
                 newEdge["speed"] = edge["speed"]
                 newEdge["stops"] = []
                 edgeId = self.getUID()
+                newEdge["id"] = edgeId
                 self.edges[edgeId] = newEdge
-                self.nodes[key]["edges"].append(edgeId)
-                self.nodes[edge["to"]]["edges"].append(edgeId)       
+                self.nodes[newKey]["edges"].append(edgeId)
+                self.nodes[newEdge["to"]]["edges"].append(edgeId)
 
-        print(self.edges[1])
-        weight = self.computeEdgeLength(self.edges[1])
-        print(weight)
         # Intializing bus stops
-        self.bus_stops = None
+        self.bus_stops = {}
 
-
+    # Generating unique id for each new item
     def getUID(self):
+        # uuid4 should be used in the final verison
         self.uids += 1
         return self.uids
-
-    def getMinNode(self, lst):
-        minimum = min(lst)
-        minIndex = lst.index(minimum)
-        return minIndex
+    
 
     def computeEdgeLength(self, edge):
+        '''
+            This funciton computes the length of a graph edge 
+        '''
         wayId = edge["way"]
-        
         ways = self.ways[wayId]
         p0 = ways[0]
         totalDist = 0
         for i in range(1, len(ways)):
             p1 = ways[i]
-            totalDist += sqrt((p0["x"] - p1["x"] )**2 + (p0["y"] - p1["y"] )**2 )
+            totalDist += sqrt((p0["x"] - p1["x"])**2 + (p0["y"] - p1["y"])**2)
             p0 = p1
         return totalDist
 
 
 
-    def pathFinder(self, node1, node2, visited, path, dist):
-        visited[node1] = True
-        path.apped(node1)
-        if (node1 == node2): return
-        minNodeId = self.getMinNode(dist)
-        node = self.nodes[minNodeId]
-        for edgeId in node["edges"]:
-            edge = self.edges[edgeId]
-            edgeLength = self.computeEdgeLength(edge)
-            if edge["from"] == node1:
-                toNode = edge["to"]
-                dist[toNode] = dist[node1] + edgeLength if dist[node1] + edgeLength < dist[toNode] else dist[toNode] 
-                pass
-            else:
-                pass
-        
+    # Shotest path calculation start ******************************************
+    # This fuction takes a list contaning nodes and gives the min node as a result 
+    def getMinNode(self, notVisited, dist):
+        minDist = 1e31
+        minNode = "-1"
+        for n in notVisited:
+            if (dist[n] < minDist):
+                minDist = dist[n]
+                minNode = n
+ 
+        return minNode
 
 
-
-    def shortest(self, node1: int, node2: int):
-        visited = [False for i in range(len(self.nodes))]
-        dist = [1e30 for i in range(len(self.nodes))]
-        dist[node1] = 0
+    def getPath(self, parentLink, node):
         path = []
-        self.pathFinder(node1, node2, visited, path, dist)
-        pass
+        while parentLink[node] != None:
+            path.append(parentLink[node]["id"])
+            if (node == parentLink[node]["from"]):
+                node = parentLink[node]["to"]
+            else : 
+                node = parentLink[node]["from"]
+        path.reverse()
+        return path
+
+    def shortest(self, node1, node2):
+        notVisited = [key for key in self.nodes]
+        parentLink = {}
+        dist = {}
+        for n in notVisited:
+            if n == node1:
+                dist[n] = 0
+                parentLink [n] = None
+            else : 
+                dist[n] = 1e30
+        for i in range(len(notVisited)):
+            node = self.getMinNode(notVisited, dist)
+            for edgeId in self.nodes[node]["edges"]:
+                edge = self.edges[edgeId]
+                edgeLength = self.computeEdgeLength(edge)
+                toNode = edge["from"]
+                if edge["from"] == node:
+                    toNode = edge["to"]
+
+                if dist[node] + edgeLength < dist[toNode]:
+                        dist[toNode] = dist[node] + edgeLength 
+                        parentLink[toNode] = edge
+
+            notVisited.remove(node)
+            if (node == node2):
+                break
+        return self.getPath(parentLink, node2)
+    # Shotest path calculation end ******************************************
 
     def closestedge(loc):
         pass
@@ -119,6 +152,8 @@ class Map:
 
 def main():
     map = Map(path="../map.json")
+    path = map.shortest(7,19)
+    print(path)
 
 
 if __name__ == "__main__":
