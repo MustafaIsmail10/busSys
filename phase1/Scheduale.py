55ab483f773e1fc0db9d6b3db29b39535a45d064
Mustafa Ismail <mostafaismael10@hotmail.com>
2023-04-16 15:24:41 +0300

    The schedule is commented

--- phase1/Scheduale.py
+++ phase1/Scheduale.py
@@ -5,6 +5,9 @@ from Line import Line
 class Schedule:
 
     def __init__(self, sys_map: Map):
+        """
+        The constructor which takes a map object as input 
+        """
         self.map = sys_map
         self.routes = {}
         self.lines = {}
@@ -12,6 +15,9 @@ class Schedule:
         self.line_count = 0
 
     def add_route(self):
+        """
+        This function adds a new empty route 
+        """
         self.route_count += 1
         new_route_id = self.route_count
         new_route = Route(self.map, new_route_id)
@@ -19,54 +25,131 @@ class Schedule:
         return new_route_id
     
     def get_route(self, routeid):
+        """
+        This function returns a route with its id as input
+        """
         return self.routes[routeid]
     
     def get_route_info(self, route_id):
+        """
+        This function returns the stops of a route taking the route id as input 
+        """
         return self.routes[route_id].get_stops()
     
     def get_routes(self):
+        """
+        This function returns all ruotes in the system
+        """
         return self.routes
+    
+
+    def del_route(self, routeid):
+        del self.routes[routeid]
+        return True
 
     def get_stops(self):
+        """
+        This function returns all bus stops in the system
+        """
         return self.map.bus_stops
 
     def add_stop(self, edgeid,  direction, percentage,  description):
+        """
+        This fucntion adds a bus stop to the system
+        """
         stopid = self.map.addstop(edgeid,  direction, percentage,  description)
         return stopid
 
     def remove_stop(self, stop_id):
+        """
+        This function removes a bus stop from the system
+        """
         self.map.delstop(stop_id)
         for r in self.routes:
             self.routes[r].del_stop(stop_id)
 
     def add_stop_to_route(self, route_id, stop_id, wait_time):
+        """
+        This function adds existing bus stop to a route
+        """
         self.routes[route_id].add_stop(stop_id, wait_time)    
 
 
     def change_stop_wait(self, route_id, stop_id, wait):
+        """
+        This fucntion changes the wait time of the bus in a specific stop in a specific route
+        """
         self.routes[route_id].change_wait(stop_id, wait)
 
     def addline(self, name, start_time, end_time, time_between_trips, routeid, description):
+        """
+        This function adds a line to the system given the information of the line and the route that it will be assigned to. 
+        """
         self.line_count += 1
         new_line = Line(self.line_count, name, start_time, end_time, time_between_trips, self.routes[routeid], description )
         self.lines[self.line_count] = new_line
         return self.line_count
 
     def removeline(self, line_id):
+        """
+        This function remove a line from the system
+        """
         del self.lines[line_id]
 
     def get_lines(self):
+        """
+        This fucntion returns all the lines in the system
+        """
         return self.lines
         
     def lineinfo(self, lineid):
+        """
+        This function returns line information
+        """
         line = self.lines[lineid]
         return line.get_info()
     
     def del_line(self, lineid):
+        """
+        This fucntion deletes a line from the system
+        """
         del self.lines[lineid]
 
     def update_line_name(self, lineid, name):
+        """
+        update line name
+        """
         self.lines[lineid].update_name(name)
+
+    def update_line_start_time(self, lineid, start_time):
+        """
+        update line start time
+        """
+        self.lines[lineid].update_start_time(start_time)
+
+
+    def update_line_end_time(self, lineid, end_time):
+        """
+        update line end time
+        """
+        self.lines[lineid].update_end_time(end_time)
+
+
+
+    def update_line_time_between_trips(self, lineid, time_between_trips):
+        """
+        update time between trips of a line
+        """
+        self.lines[lineid].update_time_between_trips(time_between_trips)
+        
+
+    def update_line_description(self, lineid, description):
+        """
+        update time between trips of a line
+        """
+        self.lines[lineid].update_description(description)
+
+
 
     def stopinfo(self, stopid):  # which lines pass by it and when
         info = {}
