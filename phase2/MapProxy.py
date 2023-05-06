94c757a6382bc73b6f1133ac5b6d97b1ba75b78c
hanElb <hanaelboghdady50@gmail.com>
2023-05-06 11:10:19 +0300

    Merged

--- phase2/MapProxy.py
+++ phase2/MapProxy.py
@@ -2,9 +2,45 @@ from phase1.Map import *
 from threading  import RLock
 from Map import Map
 
-class MapProxy(Map):
-    def __init__(self, **kwargs) -> None:
+class MapProxy():
+    def __init__(self, **kwargs):
         self.guardian = RLock()
-        super.__init__(kwargs)
+        self._map = Map(kwargs)
 
+    def synched( func):
+        def synchronize(self,**kwargs):
+            with self.lock:
+                return func(self,**kwargs)
+        return synchronize
     
+    @synched
+    def compute_edge_length(self, edge):
+        return self._map.compute_edge_length(edge)
+
+    @synched
+    def shortest(self, node1, node2):
+        return self._map.shortest(node1,node2)
+
+    @synched
+    def closestedge(self, loc):
+        return self._map.closestedge(loc)
+
+    @synched
+    def addstop(self, edgeid, direction, percentage, description):
+        return self._map.addstop(edgeid,direction,percentage, description)
+    
+    @synched
+    def delstop(self, stopid):
+        return self._map.delstop(stopid)
+
+    @synched
+    def getstop(self, stopid):
+        return self._map.getstop(stopid)
+    
+    @synched
+    def stoptimeDistance(self, stop1: int, stop2: int):
+        return self._map.stoptimeDistance(stop1,stop2)
+
+    @synched 
+    def shorteststop(self, location):
+        return self._map.shorteststop(location)
--- phase2/ScheduleProxy.py
+++ phase2/ScheduleProxy.py
@@ -5,98 +5,106 @@ import threading as th
 
 
 class ScheduleProxy():
-    def __init__(self, sys_map: Map):
-        self._schedule = Schedule.__init__(sys_map)
-        self.lock = th.Rlock()
+    def __init__(self, sys_map: Map,name, sch_id):
+        self._schedule = Schedule(sys_map,name, sch_id)
+        self.lock = th.RLock()
 
-    def synched(self, func):
-        def synchronize(*args, **kwargs):
+    def synched( func):
+        def synchronize(self,**kwargs):
             with self.lock:
-                return func(*args, **kwargs)
+                return func(self,**kwargs)
         return synchronize
 
     @synched
+    def test(self):  
+        print("using the lock")
+
+    @synched
     def add_route(self):  
-        self.schedule.add_route()
+        return self._schedule.add_route()
 
     @synched
     def get_route(self, routeid):
-        self.schedule.get_route(routeid)
+        return self._schedule.get_route(routeid)
 
     @synched
     def get_route_info(self, route_id):
-        self.schedule.get_route_info(route_id)
+        return self._schedule.get_route_info(route_id)
 
     @synched
     def get_routes(self):
-        self.schedule.get_routes()
+        return self._schedule.get_routes()
 
     @synched
     def del_route(self, routeid):
-        self.schedule.del_route(routeid)
+        return self._schedule.del_route(routeid)
 
     @synched
     def get_stops(self):     
-        self.schedule.get_stops()
+        return self._schedule.get_stops()
         
     @synched
     def add_stop(self, edgeid,  direction, percentage,  description):
-        self.schedule.add_stop(
+        return self._schedule.add_stop(
                 edgeid,  direction, percentage,  description)
 
+    @synched
     def del_stop(self, stop_id):
-        with self.lock:
-            self.schedule.del_stop(stop_id)
+        return self._schedule.del_stop(stop_id)
 
+    @synched
     def add_stop_to_route(self, route_id, stop_id, wait_time):
-        with self.lock:
-            self.schedule.add_stop_to_route(route_id, stop_id, wait_time)
+        return self._schedule.add_stop_to_route(route_id, stop_id, wait_time)
 
+    @synched
     def del_stop_from_route(self, route_id, stop_id):
-        with self.lock:
-            self.schedule.del_stop_from_route(route_id, stop_id)
+        return self._schedule.del_stop_from_route(route_id, stop_id)
 
+    @synched
     def change_stop_wait(self, route_id, stop_id, wait):
-        with self.lock:
-            self.schedule.change_stop_wait(route_id, stop_id, wait)
+        return self._schedule.change_stop_wait(route_id, stop_id, wait)
 
+    @synched
     def addline(self, name, start_time, end_time, time_between_trips, routeid, description):
-        with self.lock:
-            self.schedule.addline(name, start_time, end_time, time_between_trips, routeid, description)
+        return self._schedule.addline(name, start_time, end_time, time_between_trips, routeid, description)
 
+    @synched
     def get_lines(self):
-        with self.lock:
-            self.schedule.get_lines()
+        return self._schedule.get_lines()
 
+    @synched
     def lineinfo(self, lineid):
-        with self.lock:
-            self.schedule.lineinfo(lineid)
+        return self._schedule.lineinfo(lineid)
 
+    @synched
     def del_line(self, lineid):
-        with self.lock:
-            self.schedule.del_line(lineid)
+        return self._schedule.del_line(lineid)
 
+    @synched
     def update_line_name(self, lineid, name):
-        with self.lock:
-            self.schedule.update_line_name(lineid, name)
+        return self._schedule.update_line_name(lineid, name)
 
+    @synched
     def update_line_start_time(self, lineid, start_time):
-        with self.lock:
-            self.schedule.update_line_start_time(lineid, start_time)
+        return self._schedule.update_line_start_time(lineid, start_time)
 
+    @synched
     def update_line_end_time(self, lineid, end_time):
-        with self.lock:
-            self.schedule.update_line_end_time(lineid, end_time)
+       return self._schedule.update_line_end_time(lineid, end_time)
 
+    @synched
     def update_line_time_between_trips(self, lineid, time_between_trips):
-        with self.lock:
-            self.schedule.update_line_time_between_trips(
+        return self._schedule.update_line_time_between_trips(
                 lineid, time_between_trips)
 
+    @synched
     def update_line_description(self, lineid, description):
-        with self.lock:
-            self.schedule.update_line_description(lineid, description)
+        return self._schedule.update_line_description(lineid, description)
 
+    @synched
     def stopinfo(self, stopid):  # which lines pass by it and when
-        with self.lock:
-            self.schedule.stopinfo(stopid)
+        return self._schedule.stopinfo(stopid)
+
+kwargs = {"path": "./test/test_map.json"}
+t = ScheduleProxy(Map(**kwargs),"toto",0)
+t.test()
