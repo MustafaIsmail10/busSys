f51dddd20233be92f5b91de443509dc244ed2e8a
Mustafa Ismail <mostafaismael10@hotmail.com>
2023-05-19 10:03:28 +0300

    IT's what it is

--- phase3/busSys/urls.py
+++ phase3/busSys/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Home
+from .views import Home, login
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
+    path("login", login, name="login"),
 ]
--- phase3/busSys/views.py
+++ phase3/busSys/views.py
@@ -5,3 +5,8 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
     template_name = "home.html"
+
+
+
+def login(request):
+    return render(request, 'login.html')
